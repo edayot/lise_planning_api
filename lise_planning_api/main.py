ac0aa399370d9131f1f556d56566c93b9f3d8656
edayot <pro.e.dayot@gmail.com>
2024-02-29 15:59:03 +0100

    feat: add Dockerfile

--- lise_planning_api/main.py
+++ lise_planning_api/main.py
@@ -1,5 +1,8 @@
 import uvicorn
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 
 def main():
-    uvicorn.run("lise_planning_api.internal.app:app", host="localhost", port=8000, log_level="info", reload=True)
+    uvicorn.run("lise_planning_api.internal.app:app", host="localhost", port=8000, log_level="info", reload=os.getenv("DEBUG", False) == "True")
