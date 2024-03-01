691fa46dcb12297862fdb98e218f5aaa356d7601
edayot <pro.e.dayot@gmail.com>
2024-03-01 09:15:19 +0100

    feat: implementing cache

--- lise_planning_api/internal/app.py
+++ lise_planning_api/internal/app.py
@@ -3,6 +3,10 @@ from typing import Union
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from ics import Calendar
+from pydantic import BaseModel
+from datetime import datetime, timedelta
+from typing import Dict, Any
+import hashlib
 
 from lise_planning_api.internal.models import LiseEvent
 from lise_planning_api.internal.scraping import CreatePlanning
@@ -17,19 +21,33 @@ def read_root():
     return "A simple API to create an ICS file from a Lise event."
 
 
-@app.get("/get_ics")
-def get_ics(
-    username: str,
-    password: str,
-):
-    # example : /get_ics?username=your_username&password=your_password
-    
-    planning, events = CreatePlanning().get_all(username, password)
+class CachedResponse(BaseModel):
+    content: Any
+    timestamp: datetime
 
-    # Create the ics file
+cache: Dict[str, CachedResponse] = {}
+
+@app.get("/{username}")
+def get_ics(username: str, password: str):
+    # example : /username?password=1234
+
+    # Check cache
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    cache_key = f"{username}:{hashed_password}"
+    if cache_key in cache:
+        cached_response = cache[cache_key]
+        if datetime.now() - cached_response.timestamp < timedelta(minutes=10):
+            return StreamingResponse(cached_response.content, media_type="text/calendar", headers={"Content-Disposition": "attachment; filename=planning.ics"})
+
+
+    # Get the planning
+    planning, events = CreatePlanning().get_all(username, password)
     c = Calendar()
     for event_id, event_html in events.items():
-        event : LiseEvent = LiseEvent.from_data(event_html, [event for event in planning["events"] if event["id"] == event_id][0])
+        event: LiseEvent = LiseEvent.from_data(event_html, [event for event in planning["events"] if event["id"] == event_id][0])
         c.events.add(event.to_ics())    
 
-    return StreamingResponse(c.serialize(), media_type="text/calendar", headers={"Content-Disposition": "attachment; filename=planning.ics"})
+    serialized_content = c.serialize()
+    cache[cache_key] = CachedResponse(content=serialized_content, timestamp=datetime.now())
+
+    return StreamingResponse(serialized_content, media_type="text/calendar", headers={"Content-Disposition": "attachment; filename=planning.ics"})
